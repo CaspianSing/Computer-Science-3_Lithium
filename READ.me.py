4477aaa209c435d7221d1cbebd05a4a2cc1a30b9
CaspianSing <singmg30@wvc.pshs.edu.ph>
2026-08-06 10:26:38 +0800

    Add files via upload

---
+++ READ.me.py
@@ -0,0 +1,5 @@
+#My Name is Michael Caspian Sing, from 9 - Lithium.
+#Birthday: May 12
+#Likes: Boba Tea, Butterflies, the color Blue, Music
+#Dislikes: Darkness, Art / Engineering, Physics, ES
+#List of Friends: everyone :>
